82f77b9aa635be4e01797195935c256229059853
Brooke Bachman <bbachman1994@gmail.com>
2019-11-11 16:19:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,8 +30,7 @@ def array_literal_matrix
   ["Bruce Green", "Don Gately", "Joelle van Dyne", "Kate Gompert", "Pat M."],
   ["Gerhard Schtitt", "Hal Incandenza", "Lyle", "Mario Incandenza", "Michael Pemulis"]
 ]
-
-    # Using Array literal syntax only, build a nested array that uses the data in
+# Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
 end
