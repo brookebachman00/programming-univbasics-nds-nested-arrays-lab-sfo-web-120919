62b27f70a3dbbcaeb615d605e23453047e57693e
Brooke Bachman <bbachman1994@gmail.com>
2019-11-11 16:11:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,8 +18,8 @@ ENFIELD_TENNIS_ACADEMY = [
 
 def assembled_matrix
   [
-  ["Don Gately", "Joelle van Dyne", "Pat M.","Kate Gompert", "Bruce Green"],
-  ["Hal Incandenza", "Lyle", "Gerhard Schtitt","Mario Incandenza", "Michael Pemulis"]
+  ["Bruce Green","Don Gately", "Joelle van Dyne","Kate Gompert","Pat M."],
+  ["Gerhard Schtitt","Hal Incandenza", "Lyle", ,"Mario Incandenza", "Michael Pemulis"]
 
 ]# Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
